e6bd610c642e2f39cdb0fe0654bc2896edefaffd
Ronen Amiel <ronen.amiel@gmail.com>
2018-08-12 14:48:08 +0300

    remove comments and redundant deps

--- packages/jest-environment-yoshi-bootstrap/index.js
+++ packages/jest-environment-yoshi-bootstrap/index.js
@@ -1,50 +1,13 @@
 const path = require('path');
-// const testkit = require('wix-bootstrap-testkit');
-// const configEmitter = require('wix-config-emitter');
-// const rpcTestkit = require('wix-rpc-testkit');
 const NodeEnvironment = require('jest-environment-node');
 const project = require('yoshi/config/project');
-const {
-  // PORT,
-  // MANAGEMENT_PORT,
-  // RPC_PORT,
-  // APP_CONF_DIR,
-  getPort,
-} = require('./constants');
+const { getPort } = require('./constants');
 
 const config = require(path.join(process.cwd(), 'jest-yoshi.config.js'));
 
 module.exports = class BootstrapEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup();
-
-    // const emitter = configEmitter({
-    //   sourceFolders: ['./templates'],
-    //   targetFolder: APP_CONF_DIR,
-    // });
-
-    // this.global.rpcServer = rpcTestkit.server({
-    //   port: RPC_PORT,
-    // });
-
-    // this.global.app = testkit.app('./index', {
-    //   env: {
-    //     PORT,
-    //     APP_CONF_DIR,
-    //     MANAGEMENT_PORT,
-    //     NEW_RELIC_LOG_LEVEL: 'warn',
-    //     DEBUG: '',
-    //   },
-    // });
-
-    // await config.bootstrap.emit({
-    //   emitter,
-    //   staticsUrl: project.servers.cdn.url(),
-    //   rpcServer: this.global.rpcServer,
-    // });
-
-    // await this.global.rpcServer.start();
-    // await this.global.app.start();
 
     await config.bootstrap.setup({
       globalObject: this.global,
@@ -55,9 +18,6 @@ module.exports = class BootstrapEnvironment extends NodeEnvironment {
 
   async teardown() {
     await super.teardown();
-
-    // await this.global.app.stop();
-    // await this.global.rpcServer.stop();
 
     await config.bootstrap.teardown({
       globalObject: this.global,
