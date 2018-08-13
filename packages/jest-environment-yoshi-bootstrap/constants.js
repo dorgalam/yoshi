7099ca6195b420f55db0ef17e467c4102e287c56
Ronen Amiel <ronen.amiel@gmail.com>
2018-08-13 15:22:11 +0300

    remove comments

--- packages/jest-environment-yoshi-bootstrap/constants.js
+++ packages/jest-environment-yoshi-bootstrap/constants.js
@@ -5,8 +5,3 @@ let COUNTER = 1;
 module.exports.getPort = () => {
   return 1000 + JEST_WORKER_ID * 300 + COUNTER++;
 };
-
-// module.exports.PORT = 3100 + JEST_WORKER_ID;
-// module.exports.MANAGEMENT_PORT = 3200 + JEST_WORKER_ID;
-// module.exports.RPC_PORT = 3300 + JEST_WORKER_ID;
-// module.exports.APP_CONF_DIR = `./target/configs-${JEST_WORKER_ID}`;
